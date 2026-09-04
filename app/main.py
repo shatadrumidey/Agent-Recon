2e2bf06f877d5b62ebe93d565768bf047012fb46
shatadrumidey <shatadrumidey@gmail.com>
2026-09-05 02:20:35 +0530

    Configure production CORS

--- app/main.py
+++ app/main.py
@@ -26,7 +26,11 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "http://localhost:5173",
+        "http://localhost:3000",
+        "https://agentrecon-git-main-shata2.vercel.app",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
